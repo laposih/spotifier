3ba8376a0b60ab466b8612cff266ff6b2e13341c
helga <laposi.helga@gmail.com>
2020-10-17 14:42:07 +0200

    Initialize application

---
+++ src/main/java/org/laposi/spotifier/SpotifierApplication.java
@@ -0,0 +1,13 @@
+package org.laposi.spotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifierApplication.class, args);
+	}
+
+}
